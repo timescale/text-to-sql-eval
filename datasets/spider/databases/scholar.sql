b8aef41f622742a3273ca4fcaa18907d797acb37
Matthew Peveler <mpeveler@timescale.com>
2025-02-28 09:37:36 -0700

    add primary and foreign keys to spider.scholar
    Signed-off-by: Matthew Peveler <mpeveler@timescale.com>

--- datasets/spider/databases/scholar.sql
+++ datasets/spider/databases/scholar.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.author (
-    authorid bigint NOT NULL,
+    authorid bigint NOT NULL PRIMARY KEY,
     authorname text
 );
 
@@ -7,17 +7,8 @@ COMMENT ON COLUMN public.author.authorid IS 'Unique identifier for each author';
 
 COMMENT ON COLUMN public.author.authorname IS 'Name of the author';
 
-CREATE TABLE public.cite (
-    citingpaperid bigint NOT NULL,
-    citedpaperid bigint NOT NULL
-);
-
-COMMENT ON COLUMN public.cite.citingpaperid IS 'The ID of the paper that is doing the citing.';
-
-COMMENT ON COLUMN public.cite.citedpaperid IS 'The ID of the paper that is being cited.';
-
 CREATE TABLE public.dataset (
-    datasetid bigint NOT NULL,
+    datasetid bigint NOT NULL PRIMARY KEY,
     datasetname text
 );
 
@@ -26,13 +17,13 @@ COMMENT ON COLUMN public.dataset.datasetid IS 'Unique identifier for each datase
 COMMENT ON COLUMN public.dataset.datasetname IS 'Name of the dataset';
 
 CREATE TABLE public.field (
-    fieldid bigint
+    fieldid bigint NOT NULL PRIMARY KEY
 );
 
 COMMENT ON COLUMN public.field.fieldid IS 'Unique identifier for each field in the table';
 
 CREATE TABLE public.journal (
-    journalid bigint NOT NULL,
+    journalid bigint NOT NULL PRIMARY KEY,
     journalname text
 );
 
@@ -41,7 +32,7 @@ COMMENT ON COLUMN public.journal.journalid IS 'Unique identifier for each journa
 COMMENT ON COLUMN public.journal.journalname IS 'Name or title of the journal';
 
 CREATE TABLE public.keyphrase (
-    keyphraseid bigint NOT NULL,
+    keyphraseid bigint NOT NULL PRIMARY KEY,
     keyphrasename text
 );
 
@@ -49,14 +40,25 @@ COMMENT ON COLUMN public.keyphrase.keyphraseid IS 'Unique identifier for each ke
 
 COMMENT ON COLUMN public.keyphrase.keyphrasename IS 'The actual keyphrase text';
 
+CREATE TABLE public.venue (
+    venueid bigint NOT NULL PRIMARY KEY,
+    venuename text
+);
+
+COMMENT ON COLUMN public.venue.venueid IS 'Unique identifier for each venue';
+
+COMMENT ON COLUMN public.venue.venuename IS 'Name of the venue';
+
 CREATE TABLE public.paper (
-    paperid bigint NOT NULL,
+    paperid bigint NOT NULL PRIMARY KEY,
     title text,
     venueid bigint,
     year bigint,
     numciting bigint,
     numcitedby bigint,
-    journalid bigint
+    journalid bigint,
+    FOREIGN KEY (journalid) REFERENCES public.journal(journalid),
+    FOREIGN KEY (venueid) REFERENCES public.venue(venueid)
 );
 
 COMMENT ON COLUMN public.paper.paperid IS 'The unique ID of the paper.';
@@ -73,9 +75,23 @@ COMMENT ON COLUMN public.paper.numcitedby IS 'The number of papers that cite thi
 
 COMMENT ON COLUMN public.paper.journalid IS 'The ID of the journal where the paper was published.';
 
+CREATE TABLE public.cite (
+    citingpaperid bigint NOT NULL,
+    citedpaperid bigint NOT NULL,
+    FOREIGN KEY (citingpaperid) REFERENCES public.paper(paperid),
+    FOREIGN KEY (citedpaperid) REFERENCES public.paper(paperid)
+);
+
+COMMENT ON COLUMN public.cite.citingpaperid IS 'The ID of the paper that is doing the citing.';
+
+COMMENT ON COLUMN public.cite.citedpaperid IS 'The ID of the paper that is being cited.';
+
 CREATE TABLE public.paperdataset (
-    paperid bigint,
-    datasetid bigint
+    paperid bigint NOT NULL,
+    datasetid bigint NOT NULL,
+    PRIMARY KEY (paperid, datasetid),
+    FOREIGN KEY (paperid) REFERENCES public.paper(paperid),
+    FOREIGN KEY (datasetid) REFERENCES public.dataset(datasetid)
 );
 
 COMMENT ON COLUMN public.paperdataset.paperid IS 'Unique identifier for each paper in the dataset';
@@ -83,8 +99,11 @@ COMMENT ON COLUMN public.paperdataset.paperid IS 'Unique identifier for each pap
 COMMENT ON COLUMN public.paperdataset.datasetid IS 'Unique identifier for each dataset that the paper belongs to';
 
 CREATE TABLE public.paperfield (
-    fieldid bigint,
-    paperid bigint
+    fieldid bigint NOT NULL,
+    paperid bigint NOT NULL,
+    PRIMARY KEY (fieldid, paperid),
+    FOREIGN KEY (fieldid) REFERENCES public.field(fieldid),
+    FOREIGN KEY (paperid) REFERENCES public.paper(paperid)
 );
 
 COMMENT ON COLUMN public.paperfield.fieldid IS 'Unique identifier for each field in the table';
@@ -93,25 +112,22 @@ COMMENT ON COLUMN public.paperfield.paperid IS 'Unique identifier for each paper
 
 CREATE TABLE public.paperkeyphrase (
     paperid bigint,
-    keyphraseid bigint
+    keyphraseid bigint,
+    PRIMARY KEY (paperid, keyphraseid),
+    FOREIGN KEY (paperid) REFERENCES public.paper(paperid),
+    FOREIGN KEY (keyphraseid) REFERENCES public.keyphrase(keyphraseid)
 );
 
 COMMENT ON COLUMN public.paperkeyphrase.paperid IS 'The ID of the paper associated with the keyphrase.';
 
 COMMENT ON COLUMN public.paperkeyphrase.keyphraseid IS 'The ID of the keyphrase associated with the paper.';
 
-CREATE TABLE public.venue (
-    venueid bigint NOT NULL,
-    venuename text
-);
-
-COMMENT ON COLUMN public.venue.venueid IS 'Unique identifier for each venue';
-
-COMMENT ON COLUMN public.venue.venuename IS 'Name of the venue';
-
 CREATE TABLE public.writes (
     paperid bigint,
-    authorid bigint
+    authorid bigint,
+    PRIMARY KEY (paperid, authorid),
+    FOREIGN KEY (paperid) REFERENCES public.paper(paperid),
+    FOREIGN KEY (authorid) REFERENCES public.author(authorid)
 );
 
 COMMENT ON COLUMN public.writes.paperid IS 'The unique identifier for a paper in the writes table.';
@@ -128,16 +144,6 @@ INSERT INTO public.author VALUES (7, 'Robert Moore');
 INSERT INTO public.author VALUES (8, 'Linda Taylor');
 INSERT INTO public.author VALUES (9, 'William Anderson');
 INSERT INTO public.author VALUES (10, 'Karen Martinez');
-
-INSERT INTO public.cite VALUES (1, 2);
-INSERT INTO public.cite VALUES (2, 3);
-INSERT INTO public.cite VALUES (3, 4);
-INSERT INTO public.cite VALUES (4, 5);
-INSERT INTO public.cite VALUES (5, 1);
-INSERT INTO public.cite VALUES (3, 5);
-INSERT INTO public.cite VALUES (4, 2);
-INSERT INTO public.cite VALUES (1, 4);
-INSERT INTO public.cite VALUES (3, 1);
 
 INSERT INTO public.dataset VALUES (1, 'COVID-19 Research');
 INSERT INTO public.dataset VALUES (2, 'Machine Learning Datasets');
@@ -160,11 +166,25 @@ INSERT INTO public.keyphrase VALUES (3, 'Social Media');
 INSERT INTO public.keyphrase VALUES (4, 'COVID-19');
 INSERT INTO public.keyphrase VALUES (5, 'Mental Health');
 
+INSERT INTO public.venue VALUES (1, 'Conference on Machine Learning');
+INSERT INTO public.venue VALUES (2, 'International Journal of Climate Change');
+INSERT INTO public.venue VALUES (3, 'Social Media Analysis Workshop');
+
 INSERT INTO public.paper VALUES (1, 'A Study on Machine Learning Algorithms', 1, 2020, 2, 2, 3);
 INSERT INTO public.paper VALUES (2, 'The Effects of Climate Change on Agriculture', 1, 2020, 1, 2, 1);
 INSERT INTO public.paper VALUES (3, 'Social Media and Mental Health', 2, 2019, 3, 1, 4);
 INSERT INTO public.paper VALUES (4, 'COVID-19 Impact on Society', 1, 2020, 2, 2, 2);
 INSERT INTO public.paper VALUES (5, 'Machine Learning in Tackling Climate Change', 2, 2019, 1, 2, 3);
+
+INSERT INTO public.cite VALUES (1, 2);
+INSERT INTO public.cite VALUES (2, 3);
+INSERT INTO public.cite VALUES (3, 4);
+INSERT INTO public.cite VALUES (4, 5);
+INSERT INTO public.cite VALUES (5, 1);
+INSERT INTO public.cite VALUES (3, 5);
+INSERT INTO public.cite VALUES (4, 2);
+INSERT INTO public.cite VALUES (1, 4);
+INSERT INTO public.cite VALUES (3, 1);
 
 INSERT INTO public.paperdataset VALUES (1, 2);
 INSERT INTO public.paperdataset VALUES (2, 3);
@@ -186,10 +206,6 @@ INSERT INTO public.paperkeyphrase VALUES (3, 5);
 INSERT INTO public.paperkeyphrase VALUES (4, 4);
 INSERT INTO public.paperkeyphrase VALUES (5, 1);
 INSERT INTO public.paperkeyphrase VALUES (5, 2);
-
-INSERT INTO public.venue VALUES (1, 'Conference on Machine Learning');
-INSERT INTO public.venue VALUES (2, 'International Journal of Climate Change');
-INSERT INTO public.venue VALUES (3, 'Social Media Analysis Workshop');
 
 INSERT INTO public.writes VALUES (1, 1);
 INSERT INTO public.writes VALUES (2, 2);
